2fa65fd723dcc750ca3ef1bca525df9a941ca279
Linaezzar1 <ezzarlina@gmail.com>
2024-11-17 19:11:54 +0100

    lina4.0

--- src/app/component/main/box/box.component.ts
+++ src/app/component/main/box/box.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-box',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './box.component.css'
 })
 export class BoxComponent {
-
+  private readonly router: Router = inject(Router);
+  onPurchase(){
+    const token = localStorage.getItem('authToken');
+    if (token){
+      // Purchase logic here
+      console.log('Purchased');
+    }else{
+      this.router.navigate(['/main/auth/login']);
+    }
+  }
 }
--- src/app/component/main/header/header.component.ts
+++ src/app/component/main/header/header.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { UsersService } from '../../../services/users.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  private readonly userService: UsersService = inject(UsersService);
+  isLoggedIn(): boolean {
+    return this.userService.getLoggedIn() 
+  }
+  logout(): void {
+    this.userService.logout();
+  }
 }
