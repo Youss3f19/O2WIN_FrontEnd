63eb476c250263b71f9aca88ea71887f1f91bd37
yousseflaribi <yousseflaribi10@gmail.com>
2024-11-20 11:11:29 +0100

    youssef12.0

--- src/app/component/main/header/header.component.ts
+++ src/app/component/main/header/header.component.ts
@@ -2,31 +2,39 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { UsersService } from '../../../services/users.service';
 import { User } from '../../../models/user';
+import { BoxesService } from '../../../services/boxes.service';
+import { Product } from '../../../models/product';
+import { CanvasPanierComponent } from "../canvas-panier/canvas-panier.component";
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CanvasPanierComponent , DecimalPipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent implements OnInit  {
   private readonly userService: UsersService = inject(UsersService);
-  currentUser: User | null = null;
+  private readonly boxService: BoxesService = inject(BoxesService);
 
+  currentUser: User | null = null;
+  panier!:Product[]  
+  nbItems!: number 
 
 
   ngOnInit(): void {
     this.userService.currentUser$.subscribe((user) => {
       this.currentUser = user; 
-      console.log('Current User:', this.currentUser);
+      console.log('Current User Header:', this.currentUser);
     });
+    this.boxService.currentPanier$.subscribe((panier) => {
+      console.log('État actuel du panier :', panier);
+      this.nbItems = panier?.length || 0
+    });
+  
   }
 
-
-
-
- 
   logout(): void {
     this.userService.logout();
   }
